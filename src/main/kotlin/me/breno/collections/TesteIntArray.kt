493224bb53b3dbe529651c04e0fa85fe87e64532
Breno <brenozende@gmail.com>
2021-02-23 16:04:36 -0300

    created int array test file

--- src/main/kotlin/me/breno/collections/TesteIntArray.kt
+++ src/main/kotlin/me/breno/collections/TesteIntArray.kt
@@ -1,5 +1,37 @@
 package me.breno.collections
 
 fun main() {
-    
+    //Inicialização do array
+    val values = IntArray(5)
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    //Duas formas de imprimir cada valor do array:
+    for (value in values) { //value = 1, 7, 6, 3, 2
+        println(value)
+    }
+    values.forEach {
+        println(it)
+    }
+
+    //Para mudar a referencia da variavel de "it" para outra, nesse caso "valor":
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    //Iterando sobre os indices, e não os valores em si:
+    for (index in values.indices) { //index = 0, 1, 2, 3, 4
+        println(values[index])
+    }
+
+    //Iterando sobre o array ordenado de forma crescente pelo sort():
+    values.sort()
+    for (value in values) {
+        println(value)
+    }
+
+
 }
